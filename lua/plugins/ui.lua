7c4b324a09376bc6897d8856702ad2a573f3e98c
bitsamericas_jeremias.pajaro <jeremias.pajaro@bitsamericas.com>
2025-12-11 12:54:43 -0500

    refactor(plugins): migrate echasnovski/mini.nvim to nvim-mini
    Updated plugin references and URLs from the legacy 'echasnovski' organization to the new 'nvim-mini' organization across UI, Editor, and Markdown configurations to ensure continued updates.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -110,10 +110,10 @@ return {
   },
 
   -- Plugin: mini.nvim
-  -- URL: https://github.com/echasnovski/mini.nvim
+  -- URL: https://github.com/nvim-mini/mini.nvim
   -- Description: A collection of minimal, fast, and modular Lua plugins for Neovim.
   {
-    "echasnovski/mini.nvim",
+    "nvim-mini/mini.nvim",
     version = false, -- Use the latest version
     config = function()
       require("mini.animate").setup({
